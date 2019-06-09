16ba2a1aa2dab0c6efad98ca669faec0855957e6
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2019-06-09 15:53:04 +0200

    changes in pages & benner for responsive

--- frontend/views/site/freelance.php
+++ frontend/views/site/freelance.php
@@ -21,20 +21,18 @@ $this->registerCssFile("@web/web/css/imageheader.css", [], 'css-imageheader');
 	    
 	    <div style="margin-top: 20px">
 			<div class="width50 pad-left20"><p>Hätten Sie es gewusst? Zeitarbeit kann Ihnen ganz neue Perspektiven eröffnen. <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> macht es möglich. </p></div>
-			<div class="width50 pad-right20"><p>Zugegeben. Zeitarbeit hat nicht unbedingt den besten Ruf. Doch die sogenannte „Arbeitnehmerüberlassung“ hat für Angestellte durchaus auch Vorteile: </p></div>
+			<div class="width50 pad-right20"><p>Zugegeben. Zeitarbeit hat nicht unbedingt den besten Ruf. </p></div>
 			<div class="clear"></div>
 		</div>
 
-		<div class="box-list">
-			<div class="item">Als Berufsanfänger sammeln Sie wertvolle Erfahrungen, die Ihnen den Eintritt in die Arbeitswelt erleichtern. </div>
-            <div class="item">Nach längerer Berufsabstinenz fassen Sie durch Zeitarbeit wieder Fuß auf dem Arbeitsmarkt. </div>
-            <div class="item">Gerade älteren Arbeitssuchenden winken mit dem Zeitarbeitsmodell neue Perspektiven. </div>
-            <div class="item">Eine berufliche Umorientierung wird durch temporäre Arbeitsleistungen vereinfacht. </div>
-            <div class="item">Die Arbeitnehmerüberlassung ist in der Regel sozialversicherungspflichtig.</div>
-            <div class="item">Zeitarbeitnehmer haben dieselben Rechte wie regulär angestellte Arbeitnehmer. </div>
-            <div class="item">Sie wahren sich ein Höchstmaß an Flexibilität.</div>
-			<div class="clear"></div>
-		</div>
+		<p><strong> Doch die sogenannte „Arbeitnehmerüberlassung“ hat für Angestellte durchaus auch Vorteile:</strong></p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Als Berufsanfänger sammeln Sie wertvolle Erfahrungen, die Ihnen den Eintritt in die Arbeitswelt erleichtern. </p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Nach längerer Berufsabstinenz fassen Sie durch Zeitarbeit wieder Fuß auf dem Arbeitsmarkt. </p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Gerade älteren Arbeitssuchenden winken mit dem Zeitarbeitsmodell neue Perspektiven. </p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Eine berufliche Umorientierung wird durch temporäre Arbeitsleistungen vereinfacht. </p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Die Arbeitnehmerüberlassung ist in der Regel sozialversicherungspflichtig.</p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Zeitarbeitnehmer haben dieselben Rechte wie regulär angestellte Arbeitnehmer. </p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Sie wahren sich ein Höchstmaß an Flexibilität.</p>
 		
 		<br>
 		<p>Sie möchten mehr über das Modell der Zeitarbeit erfahren? Sprechen Sie uns an. Wir beantworten Ihre Fragen. </p>
--- frontend/views/site/whyjob2job.php
+++ frontend/views/site/whyjob2job.php
@@ -18,15 +18,14 @@ $this->registerCssFile("@web/web/css/imageheader.css", [], 'css-imageheader');
 	</div>
 
 	<div class="branch-content">
-	    <p>Maue Jobausbeute trotz monatelangem Bewerbungsstress? Meilenweit vom Traumjob entfernt? Mangelnde Flexibilität im Job? Maximaler Einsatz, minimales Gehalt? Schluss damit! Geben Sie die Jobsuche in professionelle Hände. Bei <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> sind Sie richtig, weil...</p> 
+	    <p>Maue Jobausbeute trotz monatelangem Bewerbungsstress? Meilenweit vom Traumjob entfernt? Mangelnde Flexibilität im Job? Maximaler Einsatz, minimales Gehalt? Schluss damit! Geben Sie die Jobsuche in professionelle Hände.</p>
+	    <p><strong> Bei <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> sind Sie richtig, weil...</strong></p> 
 
-		<div class="box-list">
-			<div class="item"> bei uns immer Sie im Mittelpunkt stehen. <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> betreut Sie über den gesamten Bewerbungsprozess hindurch und darüber hinaus.</div>
-			<div class="item"> Sie sich ruhig auch mal auf Ihren Lorbeeren ausruhen dürfen. Sie haben die Ausbildung, wir den passenden Job.</div>
-			<div class="item"> Sie sicherlich nicht gerne in die berufliche Sackgasse steuern. <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> reißt das Ruder herum und verschafft Ihnen neue Perspektiven.</div>
-			<div class="item"> wir in vielen Branchen bestens vernetzt sind. Damit haben Sie bei Ihrem potenziellen Arbeitgeber gleich ein Stein im Brett – und beste Chancen auf eine Anstellung in Ihrem Traumjob. </div>
-			<div class="item"> bei uns immer Sie der Boss sind. Sie entscheiden wo, wann und wie viel Sie arbeiten möchten – wir kümmern uns um den Rest. </div>
-		</div>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> bei uns immer Sie im Mittelpunkt stehen. <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> betreut Sie über den gesamten Bewerbungsprozess hindurch und darüber hinaus.</p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Sie sich ruhig auch mal auf Ihren Lorbeeren ausruhen dürfen. Sie haben die Ausbildung, wir den passenden Job.</p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> Sie sicherlich nicht gerne in die berufliche Sackgasse steuern. <a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> reißt das Ruder herum und verschafft Ihnen neue Perspektiven.</p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> wir in vielen Branchen bestens vernetzt sind. Damit haben Sie bei Ihrem potenziellen Arbeitgeber gleich ein Stein im Brett – und beste Chancen auf eine Anstellung in Ihrem Traumjob. </p>
+		<p style="border: 1px solid gray; margin: 20px 0; padding: 8px; text-align: center;"> bei uns immer Sie der Boss sind. Sie entscheiden wo, wann und wie viel Sie arbeiten möchten – wir kümmern uns um den Rest. </p>
 		<div class="clear"></div>
 		<br>
 		<p><a href="<?php echo Yii::getAlias('@web');?>"><b>Job2Job</b></a> – weil Sie sich nicht mit weniger zufrieden geben möchten!</p>
