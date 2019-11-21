31b8427cdc4e1f604b36ef4c2da92bd809bf486b
hamidrezaseifi-at-513711302859 <hamidrezaseifi@gmail.com>
2019-11-21 22:29:07 +0100

    changes

--- backend/models/EmailForm.php
+++ backend/models/EmailForm.php
@@ -44,6 +44,7 @@ class EmailForm extends Model
         $headers = array();
         $headers[] = 'MIME-Version: 1.0';
         $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+        $headers[] = 'From: Job2Job <info@job2job-gmbh.de>';
         
         return mail($this->receiver, $this->title, \Yii::$app->formatter->asNtext($this->body), implode("\r\n", $headers));
     }
